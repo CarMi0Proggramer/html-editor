dc7191d7a84706846c29e20993b54b7b9724b805
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-17 06:02:29 -0500

    Base structure added

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,12 @@
 // @ts-check
 import { defineConfig } from "astro/config";
-
 import vercel from "@astrojs/vercel";
-
 import tailwind from "@astrojs/tailwind";
+import db from "@astrojs/db";
 
 // https://astro.build/config
 export default defineConfig({
   output: "server",
   adapter: vercel(),
-  integrations: [tailwind()],
+  integrations: [tailwind(), db()],
 });
