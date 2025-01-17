40028d13e9bf04ef47c7679a9372a2beda65ebe8
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-17 06:02:14 -0500

    Astro configured

---
+++ .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
@@ -0,0 +1,1 @@
+export { c as createExports, s as start } from './chunks/entrypoint_BrDshbR9.mjs';
---
+++ .vercel/output/_functions/pages/_image.astro.mjs
@@ -0,0 +1,2 @@
+export { a as page } from '../chunks/generic_DhDJv5RV.mjs';
+export { renderers } from '../renderers.mjs';
