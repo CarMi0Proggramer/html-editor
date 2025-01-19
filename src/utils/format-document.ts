9ead30ff77a2550241f18c47827ecf17c1fffc59
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-18 22:10:10 -0500

    Utility for formatting section texts added

---
+++ src/utils/format-document.ts
@@ -0,0 +1,15 @@
+export function formatDocumentContent(content: FormDataEntryValue) {
+  // Convertir el contenido a una cadena
+  let formattedContent = content.toString();
+
+  // Reemplazar los enlaces con etiquetas anchor
+  formattedContent = formattedContent.replace(
+    /(https?:\/\/[^\s]+)/g,
+    '<a style="color: #2563eb; text-decoration: underline;" target="_blank" href="$1">$1</a>'
+  );
+
+  // Reemplazar los saltos de línea con etiquetas <br>
+  formattedContent = formattedContent.replace(/\n/g, "<br>");
+
+  return formattedContent;
+}
