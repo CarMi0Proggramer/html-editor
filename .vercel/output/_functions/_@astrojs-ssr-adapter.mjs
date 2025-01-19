73411c60ec0c726a5e43926ef2b72db5b7f5eb0b
Carlos Miguel <carlosmiguelgarcesbasulto@gmail.com>
2025-01-18 23:23:17 -0500

    .vercel deleted

--- .vercel/output/_functions/_@astrojs-ssr-adapter.mjs
+++
@@ -1,1 +0,0 @@
-export { c as createExports, s as start } from './chunks/entrypoint_BrDshbR9.mjs';
--- .vercel/output/_functions/pages/_image.astro.mjs
+++
@@ -1,2 +0,0 @@
-export { a as page } from '../chunks/generic_DhDJv5RV.mjs';
-export { renderers } from '../renderers.mjs';
